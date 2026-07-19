8f1e9b943a991e80679c59f9d68bcb5d586c6d85
Augustin Gottlieb <33221555+aguspe@users.noreply.github.com>
2026-07-20 05:46:58 +0900

    style: rubocop autocorrect skeleton

--- lib/selenium_spec.rb
+++ lib/selenium_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "selenium_spec/version"
+require_relative 'selenium_spec/version'
 
 module SeleniumSpec
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require "selenium_spec"
+require 'selenium_spec'
 
-Dir[File.join(__dir__, "support", "**", "*.rb")].each { |f| require f }
+Dir[File.join(__dir__, 'support', '**', '*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.disable_monkey_patching!
   config.expect_with(:rspec) { |c| c.syntax = :expect }
   config.order = :random
   Kernel.srand config.seed
-  config.filter_run_excluding :browser unless ENV["BROWSER_TESTS"]
+  config.filter_run_excluding :browser unless ENV['BROWSER_TESTS']
 end
