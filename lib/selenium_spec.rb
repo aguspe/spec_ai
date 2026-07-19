aabbad19f037fe6b992250f9f068f9f29bb71154
Augustin Gottlieb <33221555+aguspe@users.noreply.github.com>
2026-07-20 06:15:50 +0900

    feat: BrowserSession wrapper with error taxonomy and snapshot JS

--- lib/selenium_spec.rb
+++ lib/selenium_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "selenium_spec/version"
+require_relative "selenium_spec/errors"
+require_relative "selenium_spec/browser_session"
 require_relative "selenium_spec/recorder"
 require_relative "selenium_spec/codegen/rspec_renderer"
 require_relative "selenium_spec/codegen/capybara_renderer"
---
+++ spec/selenium_spec/browser_session_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+RSpec.describe SeleniumSpec::BrowserSession do
+  let(:fake_element) do
+    instance_double(Selenium::WebDriver::Element).tap do |el|
+      allow(el).to receive_messages(tag_name: "button", text: "Log in", click: nil)
+      allow(el).to receive(:attribute).with("id").and_return("login-btn")
+      allow(el).to receive(:attribute).with("name").and_return(nil)
+      allow(el).to receive(:attribute).with("type").and_return("submit")
+    end
+  end
+
+  let(:fake_driver) do
+    double("driver").tap do |d| # rubocop:disable RSpec/VerifiedDoubles
+      allow(d).to receive_messages(quit: nil, current_url: "https://x.test/", title: "X")
+      allow(d).to receive(:find_element).with({ id: "login-btn" }).and_return(fake_element)
+    end
+  end
+
+  let(:session) { described_class.new(driver_factory: ->(_browser, _headless) { fake_driver }) }
+
+  it "raises SessionNotStartedError before start" do
+    expect { session.navigate("https://x.test") }.to raise_error(SeleniumSpec::SessionNotStartedError)
+  end
+
+  it "starts and reports alive" do
+    session.start(browser: "chrome")
+    expect(session).to be_alive
+    expect(session.browser_name).to eq("chrome")
+  end
+
+  it "click returns element metadata captured before the click" do
+    session.start(browser: "chrome")
+    meta = session.click(%w[id login-btn])
+    expect(meta).to eq(tag: "button", text: "Log in", id: "login-btn", name: nil, type: "submit")
+  end
+
+  it "wraps NoSuchElementError into ElementNotFoundError with suggestions" do
+    session.start(browser: "chrome")
+    allow(fake_driver).to receive(:find_element).with({ id: "missing" })
+                                                .and_raise(Selenium::WebDriver::Error::NoSuchElementError)
+    session.instance_variable_set(:@last_snapshot,
+                                  [{ "tag" => "button", "id" => "login-btn", "name" => nil, "text" => "Log in" }])
+    expect { session.find(%w[id missing]) }.to raise_error(SeleniumSpec::ElementNotFoundError, /Did you mean/)
+  end
+
+  it "marks session dead on InvalidSessionIdError and raises SessionDeadError afterwards" do
+    session.start(browser: "chrome")
+    allow(fake_driver).to receive(:current_url).and_raise(Selenium::WebDriver::Error::InvalidSessionIdError)
+    expect { session.current_url }.to raise_error(SeleniumSpec::SessionDeadError)
+    expect { session.title }.to raise_error(SeleniumSpec::SessionDeadError)
+    expect(session).not_to be_alive
+  end
+
+  it "identifies password fields from metadata" do
+    expect(session.password_field?(type: "password")).to be true
+    expect(session.password_field?(type: "text")).to be false
+  end
+end
